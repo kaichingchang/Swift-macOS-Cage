42611f9549a2d43357e185dbb2b18222269a943a
張凱慶 <kaiching@zhangkaiqingdeMacBook-Pro.local>
2017-08-05 13:31:48 +0800

    Initial Commit

---
+++ Cage/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Cage
+//
+//  Created by 張凱慶 on 05/08/2017.
+//  Copyright © 2017 kaiching. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Cage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Cage
+//
+//  Created by 張凱慶 on 05/08/2017.
+//  Copyright © 2017 kaiching. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
